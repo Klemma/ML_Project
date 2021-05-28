97d2f5107a1437b2b6425466bbe1bd2b9d42de2a
Ilya <klemin.ir@gmail.com>
2021-05-28 22:24:44 +0300

    Сделан скрипт для преобразования макета в python-код

---
+++ application/ui_to_py.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == '__main__':
+    command = 'pyuic5'
+    source = './ui/design.ui'
+    target = './src/Ui_MainWindow.py'
+    flags = '-o'
+    os.system(f'{command} {source} {flags} {target}')
