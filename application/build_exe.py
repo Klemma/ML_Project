096aeb7ea131f09f7ce2607abed964b3ce4684b0
Ilya <klemin.ir@gmail.com>
2021-05-29 21:10:34 +0300

    Минорные изменения

--- application/build_exe.py
+++ application/build_exe.py
@@ -1,7 +1,7 @@
 import PyInstaller.__main__
 
 if __name__ == '__main__':
-    source = './src/MainWindow.py'
+    source = './src/gui/MainWindow.py'
     flags = ['--onefile', '--noconsole']
 
     PyInstaller.__main__.run([source, *flags])
--- application/ui_to_py.py
+++ application/ui_to_py.py
@@ -3,6 +3,6 @@ import os
 if __name__ == '__main__':
     command = 'pyuic5'
     source = './ui/design.ui'
-    target = './src/Ui_MainWindow.py'
+    target = './src/gui/Ui_MainWindow.py'
     flags = '-o'
     os.system(f'{command} {source} {flags} {target}')
