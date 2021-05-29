5f4a1142ca5b2eb6bb616d42aff681cc28fbb79d
Ilya <klemin.ir@gmail.com>
2021-05-29 21:09:55 +0300

    Сделан файл, который содержит пути к различным элементам проекта

---
+++ definitions.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(ROOT_DIR, r'models\simple_seq2seq.model')
+VOCABS_PATH = os.path.join(ROOT_DIR, r'application\src\vocab\vocabs.vcb')
