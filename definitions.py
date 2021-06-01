24bd4d46b8e2c7e23fbf62586ab3a48dd0afbf29
Ilya <klemin.ir@gmail.com>
2021-06-01 18:20:03 +0300

    Минорные изменения

--- definitions.py
+++ definitions.py
@@ -1,5 +1,2 @@
-import os
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(ROOT_DIR, r'models\simple_seq2seq.model')
-VOCABS_PATH = os.path.join(ROOT_DIR, r'application\src\vocab\vocabs.vcb')
+MODEL_PATH = './models/simple_seq2seq.model'
+VOCABS_PATH = './application/src/vocab/vocabs.vcb'
